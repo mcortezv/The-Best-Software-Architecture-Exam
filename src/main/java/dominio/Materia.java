f3f2731b810cee287eb4b44bd610065692a59481
mcortezv <mdjesuscv@gmail.com>
2026-02-25 22:39:11 -0700

    Entidad Materia

--- src/main/java/dominio/Materia.java
+++ src/main/java/dominio/Materia.java
@@ -1,4 +1,60 @@
 package dominio;
+import java.time.LocalTime;
 
 public class Materia {
+    private int codigo;
+    private String nombre;
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
+    private String dias;
+
+    public Materia() {}
+
+    public Materia(int codigo, String nombre, LocalTime horaInicio, LocalTime horaFin, String dias) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+        this.dias = dias;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDias() {
+        return dias;
+    }
+
+    public void setDias(String dias) {
+        this.dias = dias;
+    }
+
+    public LocalTime getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(LocalTime horaFin) {
+        this.horaFin = horaFin;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
