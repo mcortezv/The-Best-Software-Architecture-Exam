7c346c03e2a1ad206bbef438415226d5072dfb85
mcortezv <mdjesuscv@gmail.com>
2026-02-25 22:20:35 -0700

    Entidad Estudiante

--- src/main/java/dominio/Estudiante.java
+++ src/main/java/dominio/Estudiante.java
@@ -1,4 +1,64 @@
 package dominio;
+import java.util.List;
 
 public class Estudiante {
+    private int id;
+    private String nombre;
+    private String correo;
+    private int semestreInscrito;
+    private List<Materia> materias;
+
+    public Estudiante() {}
+
+    public Estudiante(int id, String nombre, String correo, int semestreInscrito, List<Materia> materias) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.semestreInscrito = semestreInscrito;
+        this.materias = materias;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Materia> getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(List<Materia> materias) {
+        this.materias = materias;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getSemestreInscrito() {
+        return semestreInscrito;
+    }
+
+    public void setSemestreInscrito(int semestreInscrito) {
+        this.semestreInscrito = semestreInscrito;
+    }
+
+    public int getCantidadMaterias() {
+        return materias.size();
+    }
 }
